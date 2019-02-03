b86a7c5589bc9dd3288302e569ef20f37a629e2a
CurtisMIT <curtismitmit@gmail.com>
2019-02-02 22:35:01 -0500

    score board

--- game.js
+++ game.js
@@ -8,31 +8,63 @@ let Application = PIXI.Application,
     resources = PIXI.loader.resources,
     TextureCache = PIXI.utils.TextureCache,
     Sprite = PIXI.Sprite;
+    Text = PIXI.Text;
+    TextStyle = PIXI.TextStyle;
 
 var stage = new PIXI.Container();
 document.body.appendChild(app.view);
+
 
 loader
   .add("images/cat.png")
   .load(setup);
 
 let cat;
+let score = 0;
+
+let style = new TextStyle({
+  fontFamily: "Arial",
+  fontSize: 36,
+  fill: "white",
+  stroke: '#ff3300',
+  strokeThickness: 4,
+  dropShadow: true,
+  dropShadowColor: "#000000",
+  dropShadowBlur: 4,
+  dropShadowAngle: Math.PI / 6,
+  dropShadowDistance: 6,
+});
+
+let message = new Text("SCORE: ", style);
+
+//Position it and add it to the stage
+message.position.set(1200, 70);
+app.stage.addChild(message);
+
+
 
 function setup() {
 
   //Create the `cat` sprite 
-  cat = new Sprite(resources["images/cat.png"].texture);
+  cat = new PIXI.Sprite.FromImage('https://i.gifer.com/4dI1.gif');
   cat.y = 96; 
   cat.vx = 0;
   cat.vy = 0;
   app.stage.addChild(cat);
+  
  
   //Start the game loop
   app.ticker.add(delta => gameLoop(delta));
   cat.interactive = true;
   cat.on('click', function(e){
-  console.log("touched");
-})
+    console.log("touched");
+    score = score + 1;
+    message.text = "SCORE: " + score;
+    app.stage.removeChild(cat);
+  })
+  
+
+  
 }
 
 function gameLoop(delta){
@@ -45,7 +77,18 @@ function gameLoop(delta){
   //position to make it move
   cat.x += cat.vx;
   cat.y += cat.vy;
+  creation(cat);
 }
+
+function creation(cat){
+  cat = new Sprite(resources["images/cat.png"].texture);
+  cat.y = 96; 
+  cat.vx = 0;
+  cat.vy = 0;
+  app.stage.addChild(cat);
+}
+
+
 
 
 
