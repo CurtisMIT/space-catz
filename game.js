4133256458549aac84d18313ab3318d56e978464
CurtisMIT <curtismitmit@gmail.com>
2019-02-02 21:42:51 -0500

    moving sprite

--- game.js
+++ game.js
@@ -1,13 +1,53 @@
 let app = new PIXI.Application(window.innerWidth, window.innerHeight, {backgroundColor : 0x2ad999});
 document.body.appendChild(app.view);
+app.renderer.backgroundColor = 0x061639;
 
-let stage = new PIXI.Container();
+let Application = PIXI.Application,
+    Container = PIXI.Container,
+    loader = PIXI.loader,
+    resources = PIXI.loader.resources,
+    TextureCache = PIXI.utils.TextureCache,
+    Sprite = PIXI.Sprite;
 
-let ballSize = 10;
-let circles = [];
+var stage = new PIXI.Container();
+document.body.appendChild(app.view);
 
-app.ticker.add(
-  (delta) => {
-    ballSize += 1; 
-  }
-);
+loader
+  .add("images/cat.png")
+  .load(setup);
+
+let cat;
+
+function setup() {
+
+  //Create the `cat` sprite 
+  cat = new Sprite(resources["images/cat.png"].texture);
+  cat.y = 96; 
+  cat.vx = 0;
+  cat.vy = 0;
+  app.stage.addChild(cat);
+ 
+  //Start the game loop
+  app.ticker.add(delta => gameLoop(delta));
+  cat.interactive = true;
+  cat.on('click', function(e){
+  console.log("touched");
+})
+}
+
+function gameLoop(delta){
+
+  //Update the cat's velocity
+  cat.vx = 1;
+  cat.vy = 1;
+
+  //Apply the velocity values to the cat's 
+  //position to make it move
+  cat.x += cat.vx;
+  cat.y += cat.vy;
+}
+
+
+
+
+
